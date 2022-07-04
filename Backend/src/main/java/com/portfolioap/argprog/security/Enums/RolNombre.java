741e2b5951c4782506b5162b7952da6dd794550b
ArielAlvarez95 <alvarezarielb@gmail.com>
2022-07-04 19:47:08 -0300

    Agregando Login JWT

---
+++ Backend/src/main/java/com/portfolioap/argprog/security/Enums/RolNombre.java
@@ -0,0 +1,5 @@
+package com.portfolioap.argprog.security.Enums;
+
+public enum RolNombre {
+    ROLE_ADMIN, ROLE_USER
+}
---
+++ Backend/src/main/java/com/portfolioap/argprog/security/Repository/iRolRepository.java
@@ -0,0 +1,12 @@
+package com.portfolioap.argprog.security.Repository;
+
+import com.portfolioap.argprog.security.Entity.Rol;
+import com.portfolioap.argprog.security.Enums.RolNombre;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface iRolRepository extends JpaRepository<Rol, Integer> {
+    Optional<Rol> findByRolNombre(RolNombre rolNombre);
+}
