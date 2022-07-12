f1628fa05301e7c587e5bb73922edac75b96e36f
ArielAlvarez95 <alvarezarielb@gmail.com>
2022-07-12 15:39:58 -0300

    .

--- FrontEnd/src/app/service/token.service.ts
+++ FrontEnd/src/app/service/token.service.ts
@@ -10,7 +10,7 @@ const AUTHORITIES_KEY = 'AuthAuthorities';
   providedIn: 'root'
 })
 export class TokenService {
-  roles = Array<string> = [];
+  roles: Array<string> = [];
 
   constructor() { }
 
@@ -39,13 +39,14 @@ export class TokenService {
 
   public getAuthorities(): string[]{
     this.roles = [];
-    if(sessionStorage.getItem(AUTHORITIES_KEY)!){
+    if(sessionStorage.getItem(AUTHORITIES_KEY)){
       JSON.parse(sessionStorage.getItem(AUTHORITIES_KEY)!).forEach((authority:any) => {
         this.roles.push(authority.authority);
       });
     }
-    return this.roles;
+      return this.roles;
   }
+  
   public logOut(): void{
     window.sessionStorage.clear();
   }
