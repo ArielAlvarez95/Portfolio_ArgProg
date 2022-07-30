b58431f9d86daef06be9581061bd1b0b140a4929
ArielAlvarez95 <alvarezarielb@gmail.com>
2022-07-30 02:03:59 -0300

    cambio de foto de perfil via firebase

--- FrontEnd/src/app/components/acerca-de/acerca-de.component.ts
+++ FrontEnd/src/app/components/acerca-de/acerca-de.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PersonaService } from 'src/app/service/persona.service';
 import { persona } from 'src/model/persona.model';
-import { Storage, ref, uploadBytes, listAll, getDownloadURL } from '@angular/fire/storage'
+import { Storage, ref, uploadBytes, listAll, getDownloadURL, deleteObject } from '@angular/fire/storage'
 import { TokenService } from 'src/app/service/token.service';
 
 @Component({
@@ -10,29 +10,30 @@ import { TokenService } from 'src/app/service/token.service';
   styleUrls: ['./acerca-de.component.css']
 })
 export class AcercaDeComponent implements OnInit {
-  persona: persona = new persona("","",""); 
+  persona: persona = new persona("", "", "");
   images: string[];
 
-  constructor(public personaService: PersonaService, private storage: Storage, private tokenService: TokenService) { 
+  constructor(public personaService: PersonaService, private storage: Storage, private tokenService: TokenService) {
     this.images = [];
   }
-  
-    isLogged = false;
+
+  isLogged = false;
 
   ngOnInit(): void {
-    this.personaService.getPersona().subscribe(data => {this.persona = data})
-    this.getImages(); 
-    if(this.tokenService.getToken()){
+    this.personaService.getPersona().subscribe(data => { this.persona = data })
+    this.getImages();
+    if (this.tokenService.getToken()) {
       this.isLogged = true;
       this.uploadImage;
     }
   }
 
-  uploadImage($event: any){
+  uploadImage($event: any) {
     const file = $event.target.files[0];
     console.log(file);
-
+    const imagesRef = ref(this.storage, 'images');
     const imgRef = ref(this.storage, `images/${file.name}`);
+    console.log(imgRef);
 
     uploadBytes(imgRef, file)
       .then(response => {
@@ -40,20 +41,39 @@ export class AcercaDeComponent implements OnInit {
         this.getImages();
       })
       .catch(error => console.log(error));
-    }
-
-  getImages(){
-    const imagesRef = ref(this.storage, 'images');
 
     listAll(imagesRef)
-      .then(async response =>{
-        console.log(response)
-        this.images = [];
-        for(let item of response.items){
-          const url = await getDownloadURL(item);
-          this.images.push(url);
+      .then(response => {
+        console.log(response);
+        if (response.items.length > 0) {
+          const oldImage = response.items[0]
+          const oldImg = ref(this.storage, `images/${oldImage.name}`);
+          deleteObject(oldImg)
+            .then(response => {
+              console.log(response);
+            })
+            .catch(error => console.log(error));
         }
       })
       .catch(error => console.log(error));
-    }
+  }
+
+  getImages() {
+    const imagesRef = ref(this.storage, 'images');
+
+    listAll(imagesRef)
+      .then(async response => {
+        console.log(response);
+        this.images = [];
+
+        for (let item of response.items) {
+          const url = await getDownloadURL(item);
+          this.images.push(url);
+        }
+
+      })
+      .catch(error => console.log(error));
+
+  }
+
 }
